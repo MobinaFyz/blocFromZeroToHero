bc3da7f2392205bdcd1f7e0818cdcc9eda458ebf
fatemeZahraFeyzi <samanta1393@gmail.com>
2021-08-01 11:58:29 +0430

    fix: change anonymous routes to named route

--- #3 - Flutter Bloc Concepts/lib/presentation/main.dart
+++ #3 - Flutter Bloc Concepts/lib/presentation/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_concepts/presentation/Screens/second_screen.dart';
+import 'package:flutter_bloc_concepts/presentation/Screens/third_screen.dart';
 
 import '../logic/cubit/counter_cubit.dart';
 import 'Screens/home_screen.dart';
@@ -17,11 +19,20 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: BlocProvider<CounterCubit>(
-        create: (context) => CounterCubit(),
-        child: HomeScreen(title: 'Flutter Demo Home Page'),
-      ),
+      routes: {
+        '/': (context) => HomeScreen(
+              title: "HomeScreen",
+              color: Colors.blueAccent,
+            ),
+        '/second': (context) => SecondScreen(
+              title: "SecondScreen",
+              color: Colors.redAccent,
+            ),
+        '/third': (context) => ThirdScreen(
+              title: "ThirdScreen",
+              color: Colors.greenAccent,
+            ),
+      },
     );
   }
 }
-
